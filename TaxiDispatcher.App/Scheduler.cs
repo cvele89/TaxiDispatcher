59d28608d2c2f3f6d625ade4304ca6c87ae9d770
Marko Cvetkovic <cwele89@gmail.com>
2019-06-15 12:03:44 +0200

    Introduced TaxiCompany entity.

--- TaxiDispatcher.App/Scheduler.cs
+++ TaxiDispatcher.App/Scheduler.cs
@@ -11,10 +11,14 @@ namespace TaxiDispatcher.App
 
     public class Scheduler
     {
-        protected Taxi taxi1 = new Taxi { DriverId = 1, DriverName = "Predrag", CompanyName = "Naxi", CurrentLocation = 1, PriceModifier = 10};
-        protected Taxi taxi2 = new Taxi { DriverId = 2, DriverName = "Nenad", CompanyName = "Naxi", CurrentLocation = 4, PriceModifier = 10 };
-        protected Taxi taxi3 = new Taxi { DriverId = 3, DriverName = "Dragan", CompanyName = "Alfa", CurrentLocation = 6, PriceModifier = 15 };
-        protected Taxi taxi4 = new Taxi { DriverId = 4, DriverName = "Goran", CompanyName = "Gold", CurrentLocation = 7, PriceModifier = 13 };
+        protected static TaxiCompany taxiCompany_Naxi = new TaxiCompany { CompanyName = "Naxi", PriceModifier = 10 };
+        protected static TaxiCompany taxiCompany_Alfa = new TaxiCompany { CompanyName = "Alfa", PriceModifier = 15 };
+        protected static TaxiCompany taxiCompany_Gold = new TaxiCompany { CompanyName = "Gold", PriceModifier = 13 };
+
+        protected Taxi taxi1 = new Taxi { DriverId = 1, DriverName = "Predrag", Company = taxiCompany_Naxi, CurrentLocation = 1 };
+        protected Taxi taxi2 = new Taxi { DriverId = 2, DriverName = "Nenad", Company = taxiCompany_Naxi, CurrentLocation = 4 };
+        protected Taxi taxi3 = new Taxi { DriverId = 3, DriverName = "Dragan", Company = taxiCompany_Alfa, CurrentLocation = 6 };
+        protected Taxi taxi4 = new Taxi { DriverId = 4, DriverName = "Goran", Company = taxiCompany_Gold, CurrentLocation = 7 };
 
         public Ride OrderRide(int startLocation, int endLocation, RideType rideType, DateTime time)
         {
@@ -104,7 +108,7 @@ namespace TaxiDispatcher.App
 
         private void CalculateRidePrice(Ride ride, RideType rideType, int startLocation, int endLocation, DateTime time)
         {
-            ride.Price = ride.TaxiInfo.PriceModifier * Math.Abs(startLocation - endLocation);
+            ride.Price = ride.TaxiInfo.Company.PriceModifier * Math.Abs(startLocation - endLocation);
 
             if (rideType == RideType.InterCity)
                 ride.Price *= 2;
@@ -117,19 +121,23 @@ namespace TaxiDispatcher.App
         {
             public int DriverId { get; set; }
             public string DriverName { get; set; }
-            public string CompanyName { get; set; }
             public int CurrentLocation { get; set; }
-            public int PriceModifier { get; set; } = 1;
+            public TaxiCompany Company { get; set; }
         }
 
         public class Ride
         {
             public int Id { get; set; }
+            public Taxi TaxiInfo { get; set; }
             public int StartLocation { get; set; }
             public int EndLocation { get; set; }
             public int Price { get; set; }
+        }
 
-            public Taxi TaxiInfo { get; set; }
+        public class TaxiCompany
+        {
+            public string CompanyName { get; set; }
+            public int PriceModifier { get; set; } = 1;
         }
     }
 }
